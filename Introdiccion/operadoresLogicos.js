8699f3c1c60760593a99c15aa9bf4758b859d46b
SoftimusPro <darielanielys@gmail.com>
2023-06-15 23:02:42 -0400

    operadoresLogicos

---
+++ Introdiccion/operadoresLogicos.js
@@ -0,0 +1,23 @@
+/*    Operadores Logicos
+
+    &&  And
+    ||  Or
+    !   Not
+
+*/
+//Ejemplo 1 para el operador de &&
+
+const nombre = 'Carlos';
+const edad = 18;
+const tieneEntrada = true;
+const tienePermiso = true;
+//const permitirAcceso = edad >= 18 && tieneEntrada;
+//console.log('Acceso permitido al concierto: ' + permitirAcceso);
+
+//Ejemplo #2 - OR
+// const permitirAcceso = (edad >= 18 && tieneEntrada) || (tienePermiso && tieneEntrada);
+// console.log('Acceso permitido al concierto: ' + permitirAcceso);
+
+//Ejemplo #3 - ! Not-- Invierte el valor de la variable y tambien para comprobar en este caso si tiene o no entrada
+// const variable = true
+// console.log(!variable);
